288b61818dbd224375f5a897ddddc63cf97a5349
Sam Chuang <samchuang@myhost.idv.tw>
2024-01-06 21:36:45 +0800

    加入 20221008 梯次的單元測試

---
+++ BudgetLibraryTest/BudgetTests_v20221008.cs
@@ -0,0 +1,151 @@
+using BudgetLibrary;
+using NSubstitute;
+
+namespace TestBudgetService
+{
+    public class BudgetTests_v20221008
+    {
+        private IBudgetRepo _budgetRepo;
+        private static BudgetService _testBudgetService;
+
+        [SetUp]
+        public void Setup()
+        {
+            _budgetRepo = Substitute.For<IBudgetRepo>();
+        }
+
+        [Test]
+        public void QueryOneMonth()
+        {
+            _budgetRepo.GetAll().Returns(new List<Budget>
+            {
+                new Budget
+                {
+                    YearMonth = "202210", Amount = 3100,
+                }
+                , new Budget
+                {
+                    YearMonth = "202211", Amount = 3100,
+                }
+                , new Budget
+                {
+                    YearMonth = "202212", Amount = 3100,
+                }
+                ,
+            });
+
+            _testBudgetService = new BudgetService(_budgetRepo);
+            BudgetShouldBe(3100,
+                           new DateTime(2022, 10, 1),
+                           new DateTime(2022, 10, 31));
+        }
+
+        [Test]
+        public void QueryPartialMonth()
+        {
+            _budgetRepo = NSubstitute.Substitute.For<IBudgetRepo>();
+            _budgetRepo.GetAll().Returns(new List<Budget>
+            {
+                new Budget
+                {
+                    YearMonth = "202210", Amount = 3100,
+                }
+            });
+            _testBudgetService = new BudgetService(_budgetRepo);
+            BudgetShouldBe(500,
+                           new DateTime(2022, 10, 1),
+                           new DateTime(2022, 10, 5));
+        }
+
+        [Test]
+        public void QueryCrossTwoMonth()
+        {
+            _budgetRepo = NSubstitute.Substitute.For<IBudgetRepo>();
+            _budgetRepo.GetAll().Returns(new List<Budget>
+            {
+                new Budget
+                {
+                    YearMonth = "202210", Amount = 310,
+                }
+                , new Budget
+                {
+                    YearMonth = "202211", Amount = 3000,
+                }
+            });
+
+            _testBudgetService = new BudgetService(_budgetRepo);
+            BudgetShouldBe(520,
+                           new DateTime(2022, 10, 30),
+                           new DateTime(2022, 11, 5));
+        }
+
+        [Test]
+        public void QueryCrossThreeMonth()
+        {
+            _budgetRepo = NSubstitute.Substitute.For<IBudgetRepo>();
+            _budgetRepo.GetAll().Returns(new List<Budget>
+            {
+                new Budget
+                {
+                    YearMonth = "202210", Amount = 310,
+                }
+                , new Budget
+                {
+                    YearMonth = "202211", Amount = 3000,
+                }
+                , new Budget
+                {
+                    YearMonth = "202212", Amount = 31,
+                }
+            });
+            _testBudgetService = new BudgetService(_budgetRepo);
+            BudgetShouldBe(3023,
+                           new DateTime(2022, 10, 30), new DateTime(2022, 12, 3));
+        }
+
+        private static void BudgetShouldBe(int expected, DateTime startDate, DateTime endDate)
+        {
+            Assert.AreEqual(_testBudgetService.Query(startDate,
+                                                     endDate), expected);
+        }
+
+        [Test]
+        public void QueryInvalidDate()
+        {
+            _budgetRepo = NSubstitute.Substitute.For<IBudgetRepo>();
+            _budgetRepo.GetAll().Returns(new List<Budget>
+            {
+                new Budget
+                {
+                    YearMonth = "202210", Amount = 310,
+                }
+                , new Budget
+                {
+                    YearMonth = "202211", Amount = 3000,
+                }
+                , new Budget
+                {
+                    YearMonth = "202212", Amount = 31,
+                }
+            });
+            _testBudgetService = new BudgetService(_budgetRepo);
+            BudgetShouldBe(0,
+                           new DateTime(2022, 12, 30),
+                           new DateTime(2022, 12, 3));
+        }
+
+        [Test]
+        public void QueryNoBudgetData()
+        {
+            _budgetRepo = NSubstitute.Substitute.For<IBudgetRepo>();
+            _budgetRepo.GetAll().Returns(new List<Budget>
+            {
+            });
+
+            _testBudgetService = new BudgetService(_budgetRepo);
+            BudgetShouldBe(0,
+                           new DateTime(2022, 1, 1),
+                           new DateTime(2022, 1, 30));
+        }
+    }
+}
